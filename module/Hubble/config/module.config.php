72c7c0e76c9c1d5e3229c2f98cd41e4f324c1658
Joe Haines <hello@joehaines.co.uk>
2015-06-12 22:27:37 +0100

    Use twig templates for 404s & errors

--- module/Hubble/config/module.config.php
+++ module/Hubble/config/module.config.php
@@ -85,8 +85,8 @@ return array(
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.twig',
             'hubble/index/index' => __DIR__ . '/../view/hubble/index/index.twig',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.twig',
+            'error/index'             => __DIR__ . '/../view/error/error.twig',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
