7213f58dc76b2cb0349c4bff8dbcf6503b6d56b2
Joe Haines <hello@joehaines.co.uk>
2015-06-13 08:44:43 +0100

    Update routing and add doctrine

--- module/Hubble/config/module.config.php
+++ module/Hubble/config/module.config.php
@@ -10,16 +10,29 @@
 return array(
     'router' => array(
         'routes' => array(
-            'hubble' => array(
-                'type' => 'segment',
+            'branches' => array(
+                'type' => 'literal',
                 'options' => array(
-                    'route'    => '/[:action]',
-                    'constraints' => array(
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                    ),
+                    'route' => '/',
                     'defaults' => array(
                         'controller' => 'Hubble\Controller\BranchList',
                         'action' => 'all',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'filters' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route'    => 'branches[/:action]',
+                            'constraints' => array(
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                                'controller' => 'Hubble\Controller\BranchList',
+                                'action' => 'all',
+                            ),
+                        ),
                     ),
                 ),
             ),
@@ -41,10 +54,10 @@ return array(
     ),
     'view_manager' => array(
         'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
         'template_map' => array(
             'layout/layout' => __DIR__ . '/../view/layout/layout.twig',
             'hubble/branch-list/unreleased' => __DIR__ . '/../view/hubble/branchlist/index.twig',
@@ -61,6 +74,21 @@ return array(
     'console' => array(
         'router' => array(
             'routes' => array(
+            ),
+        ),
+    ),
+
+    'doctrine' => array(
+        'driver' => array(
+            'Hubble_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/Hubble/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'Hubble\Entity' => 'Hubble_driver'
+                ),
             ),
         ),
     ),
